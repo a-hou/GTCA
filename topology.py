85691933f6945af3b8a93241258406fcbca311a6
Xinkai Wei <75148068+a-hou@users.noreply.github.com>
2025-01-13 15:11:17 +0800

    topology.py

--- topology.py
+++ topology.py
@@ -4,7 +4,7 @@ import numpy as np
 from knn import get_top_k_closest_and_furthest
 from utils import load_network_data
 
-adj1, features, Y = load_network_data('WikiCS')
+adj1, features, Y = load_network_data('cora')
 G = nx.from_scipy_sparse_matrix(adj1)
 shortest_path_lengths = dict(nx.all_pairs_shortest_path_length(G))
 num_nodes = features.shape[0]
@@ -15,6 +15,6 @@ for node, lengths in shortest_path_lengths.items():
     for target, distance in lengths.items():
         distance_matrix[node, target] = distance
 
-np.save('distance_matrix_WikiCS.npy', distance_matrix)
+np.save('distance_matrix_cora.npy', distance_matrix)
 
 
